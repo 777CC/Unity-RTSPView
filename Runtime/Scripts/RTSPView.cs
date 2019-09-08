c8385d9fa29f1ef7dd77cc389d956d95fb212d44
777CC <chain777@hotmail.com>
2019-09-08 14:17:28 +0700

    create

---
+++ Runtime/Scripts/RTSPView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMP;
+
+public class RTSPView : View
+{
+    [SerializeField]
+    private UniversalMediaPlayer player;
+    const string rtspScheme = "rtsp://";
+    public override void Play(string path)
+    {
+        base.Play(path);
+        player.Path = rtspScheme + path;
+        player.Play();
+        Debug.Log("Startttt : " + path);
+    }
+}
